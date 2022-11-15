ccb52723628203e5f2c606c0fae08a32139e3067
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-11-14 21:52:16 -0700

    hover tests complete

--- src/pages/summary/test/summaryForm.test.jsx
+++ src/pages/summary/test/summaryForm.test.jsx
@@ -1,7 +1,22 @@
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
-const { render, screen, fireEvent } = require("@testing-library/react");
+const { render, screen } = require("@testing-library/react");
 
-test("Checkbox is unchecked by default, checking enables the button, unchecking disables it", () => {
+test("initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: "I agree to Terms and Conditions",
+  });
+  //Checkbox unchecked by defualt, button disabled
+  expect(checkbox).not.toBeChecked();
+  const confirmOrderButton = screen.getByRole("button", {
+    name: "Confirm order",
+  });
+  expect(confirmOrderButton).toBeDisabled();
+});
+
+test("Checkbox is unchecked by default, checking enables the button, unchecking disables it", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: "I agree to Terms and Conditions",
@@ -9,15 +24,32 @@ test("Checkbox is unchecked by default, checking enables the button, unchecking 
   const confirmOrderButton = screen.getByRole("button", {
     name: "Confirm order",
   });
-  expect(confirmOrderButton).toBeDisabled();
-  //Checkbox unchecked by defualt, button disabled
-  expect(checkbox).not.toBeChecked();
 
   //Checking checkbox enables button
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmOrderButton).toBeEnabled();
 
   //Unchecking checkbox again disables button
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmOrderButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+  //popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover disappears when mout out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
