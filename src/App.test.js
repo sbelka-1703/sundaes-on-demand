2891dc8998a919969a5bb1aad12c8c73ce9d0405
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-11-23 10:56:24 -0700

    added mock server

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, logRoles } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  // screen.debug();
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  logRoles(container);
+  // console.log(container);
+  screen.logTestingPlaygroundURL();
 });
